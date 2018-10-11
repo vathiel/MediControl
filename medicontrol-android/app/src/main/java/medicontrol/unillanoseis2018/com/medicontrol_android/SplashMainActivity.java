692239cf91c280a34d92b3540cbe8badbdf20acf
diegoasencio96 <diegoasencio96@gmail.com>
2018-10-11 01:05:48 -0500

    Proyecto Android

---
+++ medicontrol-android/app/src/main/java/medicontrol/unillanoseis2018/com/medicontrol_android/SplashMainActivity.java
@@ -0,0 +1,13 @@
+package medicontrol.unillanoseis2018.com.medicontrol_android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_main);
+    }
+}
