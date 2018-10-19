25e023dd73d2b560ebc026c7f5c7055a51bfaee9
diegoasencio96 <diegoasencio96@gmail.com>
2018-10-18 23:06:06 -0500

    nuevos modelos y mejoras al API

--- medicontrol-backend/api/admin.py
+++ medicontrol-backend/api/admin.py
@@ -10,14 +10,22 @@ class FarmaciaAdmin(admin.ModelAdmin):
     list_filter = ['nombre']
     list_display =  ['nombre', 'reputacion', 'direccion', 'telefono', 'latitud', 'longitud', 'horario_atencion','estado']
 
-class FarmaciaAdmin(admin.ModelAdmin):
+class ComentarioFarmaciaAdmin(admin.ModelAdmin):
+    search_fields = ['farmacia']
+    list_filter = ['farmacia']
+    list_display =  ['farmacia', 'observacion', 'calificacion', 'estado']
+
+class TnovedadAdmin(admin.ModelAdmin):
     search_fields = ['nombre']
     list_filter = ['nombre']
-    list_display =  ['nombre', 'reputacion', 'direccion', 'telefono', 'latitud', 'longitud', 'horario_atencion','estado']
+    list_display =  ['nombre', 'estado']
 
-
+class NovedadAdmin(admin.ModelAdmin):
+    search_fields = ['farmacia', 'consecutivo', 'registro_invima']
+    list_filter = ['farmacia']
+    list_display =  ['farmacia', 'tnovedad', 'registro_invima', 'expediente', 'consecutivo', 'observacion','estado']
 
 admin.site.register(Farmacia, FarmaciaAdmin)
-admin.site.register(ComentarioFarmacia)
-admin.site.register(Tnovedad)
-admin.site.register(Novedad)
+admin.site.register(ComentarioFarmacia, ComentarioFarmaciaAdmin)
+admin.site.register(Tnovedad, TnovedadAdmin)
+admin.site.register(Novedad, NovedadAdmin)
